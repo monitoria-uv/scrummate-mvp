4c604348f5efe5c931714618b0c00d8de7066f35
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2025-04-19 19:57:13 -0500

    chore: update devDependencies for testing libraries and environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,38 @@
-module.exports = {
+// jest.config.ts
+const nextJest = require('next/jest');
+
+/**
+ * Este es el método de `next/jest` para crear una configuración extendida con soporte para:
+ * - TypeScript
+ * - JSX
+ * - App Router de Next.js
+ * - Alias como "@/components"
+ */
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
   testTimeout: 10000,
   testPathIgnorePatterns: ['/node_modules/'],
-
-  // For TypeScript
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom', // Cambiado a jsdom para pruebas con DOM
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/context/(.*)$': '<rootDir>/context/$1',
+    '^@/db/(.*)$': '<rootDir>/db/$1',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
   },
 
-  // Config to load env variables
-  setupFiles: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['components/ui/**/*.{js,jsx,ts,tsx}', 'lib/**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/components/ui/(?!features)'],
+  coverageProvider: 'v8',
 };
+
+module.exports = createJestConfig(customJestConfig);
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,19 @@
 import dotenv from 'dotenv';
+import "@testing-library/jest-dom";
+
 dotenv.config();
+
+if (typeof window !== 'undefined' && typeof window.ResizeObserver === 'undefined') {
+    // Mock ResizeObserver si no está definido (para evitar errores con @radix-ui)
+    window.ResizeObserver = class {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    };
+  }
+
+Object.defineProperty(window.HTMLElement.prototype, "scrollIntoView", {
+  configurable: true,
+  value: () => {},
+});
+
